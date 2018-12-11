cf7f3f667ce58645ae991bb792dc9b94ef893cd3
adamculp <adamculp@uws.net>
2018-12-10 19:01:46 -0500

    Added note about where to find routes, due to Delegator usage.

--- config/routes.php
+++ config/routes.php
@@ -7,32 +7,39 @@ use Zend\Expressive\Application;
 use Zend\Expressive\MiddlewareFactory;
 
 /**
-* Setup routes with a single request method:
-*
-* $app->get('/', App\Handler\HomePageHandler::class, 'home');
-* $app->post('/album', App\Handler\AlbumCreateHandler::class, 'album.create');
-* $app->put('/album/:id', App\Handler\AlbumUpdateHandler::class, 'album.put');
-* $app->patch('/album/:id', App\Handler\AlbumUpdateHandler::class, 'album.patch');
-* $app->delete('/album/:id', App\Handler\AlbumDeleteHandler::class, 'album.delete');
-*
-* Or with multiple request methods:
-*
-* $app->route('/contact', App\Handler\ContactHandler::class, ['GET', 'POST', ...], 'contact');
-*
-* Or handling all request methods:
-*
-* $app->route('/contact', App\Handler\ContactHandler::class)->setName('contact');
-*
-* or:
-*
-* $app->route(
-*     '/contact',
-*     App\Handler\ContactHandler::class,
-*     Zend\Expressive\Router\Route::HTTP_METHOD_ANY,
-*     'contact'
-* );
-*/
+ * Setup routes with a single request method:
+ *
+ * $app->get('/', App\Handler\HomePageHandler::class, 'home');
+ * $app->post('/album', App\Handler\AlbumCreateHandler::class, 'album.create');
+ * $app->put('/album/:id', App\Handler\AlbumUpdateHandler::class, 'album.put');
+ * $app->patch('/album/:id', App\Handler\AlbumUpdateHandler::class, 'album.patch');
+ * $app->delete('/album/:id', App\Handler\AlbumDeleteHandler::class, 'album.delete');
+ *
+ * Or with multiple request methods:
+ *
+ * $app->route('/contact', App\Handler\ContactHandler::class, ['GET', 'POST', ...], 'contact');
+ *
+ * Or handling all request methods:
+ *
+ * $app->route('/contact', App\Handler\ContactHandler::class)->setName('contact');
+ *
+ * or:
+ *
+ * $app->route(
+ *     '/contact',
+ *     App\Handler\ContactHandler::class,
+ *     Zend\Expressive\Router\Route::HTTP_METHOD_ANY,
+ *     'contact'
+ * );
+ * 
+ * @param Application $app
+ * @param MiddlewareFactory $factory
+ * @param ContainerInterface $container
+ */
 return function (Application $app, MiddlewareFactory $factory, ContainerInterface $container) : void {
-$app->get('/', App\Handler\HomePageHandler::class, 'home');
-$app->get('/api/ping', App\Handler\PingHandler::class, 'api.ping');
+    
+    // See individual RoutesDelegator.php within each module for routes
+    
+    //$app->get('/', App\Handler\HomePageHandler::class, 'home');
+    //$app->get('/api/ping', App\Handler\PingHandler::class, 'api.ping');
 };
