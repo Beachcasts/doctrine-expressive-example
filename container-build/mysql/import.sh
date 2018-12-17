6fe434b6870ae8a79048fb8fdc442c35d3a75241
Adam Culp <adamculp@uws.net>
2018-12-16 21:57:42 -0500

    Updated Docker setup scripts to have a working Xdebug setup. Also commented out files not needed, for later deprecation.

--- container-build/mysql/import.sh
+++ container-build/mysql/import.sh
@@ -1,2 +1,5 @@
-exec mysql -u root -p654321 -h 127.0.0.1 -P 6603 -e 'CREATE DATABASE IF NOT EXISTS dbname;'
-exec mysql -u root -p654321 -h 127.0.0.1 -P 6603 dbname < data/schema.sql
+
+# NOT USED AT THE MOMENT
+
+#exec mysql -u root -p654321 -h 127.0.0.1 -P 6603 -e 'CREATE DATABASE IF NOT EXISTS dbname;'
+#exec mysql -u root -p654321 -h 127.0.0.1 -P 6603 dbname < data/schema.sql
